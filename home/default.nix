365f94f5d3f9f00ef99451a2c6caedc14eea0cfd
Jacob Hilker <jacob.hilker2@gmail.com>
2024-03-15 19:43:50 -0400

    re-added bibulous to home.packages, fix pkg url

--- home/default.nix
+++ home/default.nix
@@ -15,14 +15,15 @@
   programs.home-manager.enable = true;
   home.packages = let
     extraNodePkgs = pkgs.callPackage ../packages/node { };
+    extraPythonPkgs = pkgs.callPackage ../packages/python { };
     # Fix any corruptions in the local copy.
     myGitFix = pkgs.writeShellScriptBin "git-fix" ''
-      if [ -d .git/objects/ ]; then
-      find .git/objects/ -type f -empty | xargs rm -f
-      git fetch -p
-      git fsck --full
-      fi
-      exit 1
+    if [ -d .git/objects/ ]; then
+    find .git/objects/ -type f -empty | xargs rm -f
+    git fetch -p
+    git fsck --full
+    fi
+    exit 1
     '';
   in with pkgs; [
     extraNodePkgs.tailwindcss-document-cli
@@ -31,34 +32,35 @@
     auctex
     zlib
     (python3.withPackages (p:
-      with p; [
-        fontforge
-        numpy
-        pandas
-        flask
-        virtualenvwrapper
-        pip
+    with p; [
+      fontforge
+      numpy
+      pandas
+      flask
+      virtualenvwrapper
+      pip
         # httpx
         pygobject3
       ]))
+      extraPythonPkgs.bibulous
       act
       tldr
-    wakatime
-    ttfautohint
-    nodePackages.pyright
-    nodejs
-    nodePackages.npm
-    nodePackages.tailwindcss
-    nodePackages.postcss-cli
-    nodePackages.typescript
-    nodePackages.degit
-    nodePackages.postcss
-    rustc
-    cargo
-    go
-    thefuck
-    jq
-    pup
+      wakatime
+      ttfautohint
+      nodePackages.pyright
+      nodejs
+      nodePackages.npm
+      nodePackages.tailwindcss
+      nodePackages.postcss-cli
+      nodePackages.typescript
+      nodePackages.degit
+      nodePackages.postcss
+      rustc
+      cargo
+      go
+      thefuck
+      jq
+      pup
     #libby
     gcc
     binutils
@@ -121,95 +123,95 @@
       sha256 = "sha256-97u8yNyfN9vbv4JAvqgFekno3dUyYu265TvpU3ZZqg4=";
     };
 
-  xdg.dataFile."fzf/plugins/fzf-marks" = {
-    source = pkgs.fetchFromGitHub {
-      owner = "urbainvaes";
-      repo = "fzf-marks";
-      rev = "ff3307287bba5a41bf077ac94ce636a34ed56d32";
-      sha256 = "12bln3pqznj4x906cxv9n9qb0m3wry8lrspqqc8b6jklydwg9b3v";
+    xdg.dataFile."fzf/plugins/fzf-marks" = {
+      source = pkgs.fetchFromGitHub {
+        owner = "urbainvaes";
+        repo = "fzf-marks";
+        rev = "ff3307287bba5a41bf077ac94ce636a34ed56d32";
+        sha256 = "12bln3pqznj4x906cxv9n9qb0m3wry8lrspqqc8b6jklydwg9b3v";
+      };
+      recursive = true;
     };
-    recursive = true;
-  };
 
-  home.sessionVariables = { "GRAPHITE_IGNORE_GIT_VERSION" = "1"; };
+    home.sessionVariables = { "GRAPHITE_IGNORE_GIT_VERSION" = "1"; };
 
-  programs.bash = {
-    enable = true;
-    shellAliases = {
-      ref = "source ~/.bashrc";
-      ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
-      cat = "${pkgs.bat}/bin/bat -p";
-    };
-    initExtra = ''
+    programs.bash = {
+      enable = true;
+      shellAliases = {
+        ref = "source ~/.bashrc";
+        ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
+        cat = "${pkgs.bat}/bin/bat -p";
+      };
+      initExtra = ''
       source ${config.xdg.dataHome}/fzf/plugins/fzf-marks/fzf-marks.plugin.bash
       function gi {
         toIgnore="$(curl -sLw "\n" https://www.toptal.com/developers/gitignore/api/list | sed 's/,/\n/g' | fzf -m | xargs | sed 's/\s/,/g')"
         curl -sL "https://www.toptal.com/developers/gitignore/api/$toIgnore" >> .gitignore
       }
-    '';
-  };
-
-  programs.emacs = {
-    enable = true;
-    package = pkgs.emacs29;
-  };
-  services.emacs = {
-    enable = true;
-    package = pkgs.emacs29;
-  };
-  xdg.configFile."doom" = {
-    source = ./config/doom;
-    recursive = true;
-  };
-
-  home.sessionVariables."DOOMDIR" = "$HOME/.dotfiles/home/config/doom";
-
-  programs.git = {
-    enable = true;
-    userName = "Jacob Hilker";
-    userEmail = "jacob.hilker2@gmail.com";
-    signing = {
-      key = "jacob.hilker2@gmail.com";
-      signByDefault = true;
+      '';
     };
-    aliases = {
-      "cleanup" =
-        "grep  -v '\\*\\|master\\|develop\\|dev\\|main' | xargs -n 1 -r git branch -d";
+
+    programs.emacs = {
+      enable = true;
+      package = pkgs.emacs29;
     };
-    delta.enable = true;
-    extraConfig = {
-      init = { defaultBranch = "main"; };
-      core = { editor = "nvim"; };
-      push = { autoSetupRemote = true; };
-      color = { ui = true; };
-      format = {
-        pretty =
-          "%C(italic)(%h)%Creset %C(bold 12)%an%Creset: %C(3)%s %Creset(%ad)";
+    services.emacs = {
+      enable = true;
+      package = pkgs.emacs29;
+    };
+    xdg.configFile."doom" = {
+      source = ./config/doom;
+      recursive = true;
+    };
+
+    home.sessionVariables."DOOMDIR" = "$HOME/.dotfiles/home/config/doom";
+
+    programs.git = {
+      enable = true;
+      userName = "Jacob Hilker";
+      userEmail = "jacob.hilker2@gmail.com";
+      signing = {
+        key = "jacob.hilker2@gmail.com";
+        signByDefault = true;
       };
-    };
-    ignores = [ "result" "result-*" "node_modules/" ];
-  };
+      aliases = {
+        "cleanup" =
+          "grep  -v '\\*\\|master\\|develop\\|dev\\|main' | xargs -n 1 -r git branch -d";
+        };
+        delta.enable = true;
+        extraConfig = {
+          init = { defaultBranch = "main"; };
+          core = { editor = "nvim"; };
+          push = { autoSetupRemote = true; };
+          color = { ui = true; };
+          format = {
+            pretty =
+              "%C(italic)(%h)%Creset %C(bold 12)%an%Creset: %C(3)%s %Creset(%ad)";
+            };
+          };
+          ignores = [ "result" "result-*" "node_modules/" ];
+        };
 
-  programs.neovim = {
-    vimAlias = true;
-    viAlias = true;
-  };
+        programs.neovim = {
+          vimAlias = true;
+          viAlias = true;
+        };
 
-  programs.nixvim = {
-    enable = true;
-    options = {
-      number = true; # Show line numbers
-      relativenumber = true; # Show relative line numbers
-      shiftwidth = 2; # Tab width should be 2
-      termguicolors = true;
-    };
+        programs.nixvim = {
+          enable = true;
+          options = {
+            number = true; # Show line numbers
+            relativenumber = true; # Show relative line numbers
+            shiftwidth = 2; # Tab width should be 2
+            termguicolors = true;
+          };
     # command! -bang -nargs=* Rg call fzf#vim#grep("rg --column --line-number --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)
     userCommands = {
       FindInFile = {
         bang = true;
         nargs = "*";
         command = ''
-          call fzf#vim#grep("rg --column --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)'';
+        call fzf#vim#grep("rg --column --no-heading --color=always --smart-case ".shellescape(<q-args>), 1, {'options': '--delimiter : --nth 4..'}, <bang>0)'';
       };
     };
     extraPlugins = with pkgs.vimPlugins; [ nvim-fzf nvim-fzf-commands fzf-vim ];
@@ -300,32 +302,32 @@
     templates =
       let templates = pkgs.callPackage ../packages/pandoc/templates.nix { };
       in { "eisvogel.latex" = "${templates.eisvogelTemplate}/eisvogel.tex"; };
-  };
-
-  programs.vscode = {
-    enable = true;
-    extensions = with pkgs.vscode-extensions; [
-      vscodevim.vim
-      jdinhlife.gruvbox
-      vspacecode.vspacecode
-    ];
-  };
-
-  programs.zsh = {
-    enable = true;
-    syntaxHighlighting.enable = true;
-    enableAutosuggestions = true;
-    autocd = true;
-    dotDir = ".config/zsh";
-
-    shellAliases = {
-      ref = "source ~/.config/zsh/.zshrc";
-      vim = "nvim $@";
-      ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
-      cat = "${pkgs.bat}/bin/bat -p";
     };
-    initExtra = let fzf-tab = pkgs.callPackage ../packages/fzf-tab.nix { };
-    in ''
+
+    programs.vscode = {
+      enable = true;
+      extensions = with pkgs.vscode-extensions; [
+        vscodevim.vim
+        jdinhlife.gruvbox
+        vspacecode.vspacecode
+      ];
+    };
+
+    programs.zsh = {
+      enable = true;
+      syntaxHighlighting.enable = true;
+      enableAutosuggestions = true;
+      autocd = true;
+      dotDir = ".config/zsh";
+
+      shellAliases = {
+        ref = "source ~/.config/zsh/.zshrc";
+        vim = "nvim $@";
+        ls = "${pkgs.eza}/bin/eza -alh --group-directories-first";
+        cat = "${pkgs.bat}/bin/bat -p";
+      };
+      initExtra = let fzf-tab = pkgs.callPackage ../packages/fzf-tab.nix { };
+      in ''
       eval $(${pkgs.thefuck}/bin/thefuck --alias)
       source ${pkgs.python3Packages.virtualenvwrapper}/bin/virtualenvwrapper.sh
       source ${fzf-tab}/fzf-tab.plugin.zsh
@@ -337,65 +339,65 @@
       function toWorkOn(){
         workon $(lsvirtualenv -b | fzf)
       }
-    '';
+      '';
 
-    dirHashes = {
-      dotfiles = "$HOME/.dotfiles/";
-      org = "$HOME/Dropbox/org/";
-      roam = "$HOME/Dropbox/roam/";
-      cetragore = "$HOME/Devel/sites/cetragore";
+      dirHashes = {
+        dotfiles = "$HOME/.dotfiles/";
+        org = "$HOME/Dropbox/org/";
+        roam = "$HOME/Dropbox/roam/";
+        cetragore = "$HOME/Devel/sites/cetragore";
+      };
     };
-  };
 
-  programs.fish.enable = true;
+    programs.fish.enable = true;
 
-  programs.starship = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
-    settings = { line_break.disabled = true; };
-  };
-
-  programs.fzf = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-    enableFishIntegration = true;
-    tmux.enableShellIntegration = true;
-  };
-
-  programs.alacritty = {
-    enable = true;
-    settings = { opacity = 0.8; };
-  };
-
-  programs.foot = {
-    enable = true;
-    settings = {
-
+    programs.starship = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      settings = { line_break.disabled = true; };
     };
-  };
 
-  programs.direnv = {
-    enable = true;
-    enableBashIntegration = true;
-    enableZshIntegration = true;
-  };
+    programs.fzf = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+      enableFishIntegration = true;
+      tmux.enableShellIntegration = true;
+    };
 
-  programs.zathura = { enable = true; };
+    programs.alacritty = {
+      enable = true;
+      settings = { opacity = 0.8; };
+    };
 
-  programs.ncspot = { enable = true; };
+    programs.foot = {
+      enable = true;
+      settings = {
 
-  programs.password-store = {
-    enable = true;
-    package = pkgs.pass.withExtensions
+      };
+    };
+
+    programs.direnv = {
+      enable = true;
+      enableBashIntegration = true;
+      enableZshIntegration = true;
+    };
+
+    programs.zathura = { enable = true; };
+
+    programs.ncspot = { enable = true; };
+
+    programs.password-store = {
+      enable = true;
+      package = pkgs.pass.withExtensions
       (exts: [ exts.pass-otp exts.pass-checkup exts.pass-import ]);
-    settings = {
-      PASSWORD_STORE_DIR = "${config.xdg.dataHome}/.password-store";
+      settings = {
+        PASSWORD_STORE_DIR = "${config.xdg.dataHome}/.password-store";
+      };
     };
-  };
 
-  services.waterNotifier = { enable = true; };
+    services.waterNotifier = { enable = true; };
 
-}
+  }
--- packages/python/default.nix
+++ packages/python/default.nix
@@ -5,7 +5,7 @@
     pname = "bibulous";
     version = "2.0";
     src = fetchFromGitHub {
-      owner = "nhagen";
+      owner = "nzhagen";
       repo = "bibulous";
       rev = "ebf1e6ce45991897d86466d60dfc0b49bc32cf8d";
       sha256 = "19ibs4ll31kw8b8mg8g9g77wgzrhchw97kw79flnf5ysbna1ailx";
